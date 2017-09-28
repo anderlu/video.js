bdeafad66dd5a9eb5e5b6a82500619b360fee510
anderlu <anderlublaze@gmail.com>
2017-09-28 20:28:34 +0800

    update demo

--- src/js/tcplayer.js
+++ src/js/tcplayer.js
@@ -6,9 +6,9 @@
 import videojs from './video.js';
 import window from 'global/window';
 
+window.videojs = videojs;
 function TcPlayer(id, options, ready) {
   // TODO
-  window.videojs = videojs;
   // resourceLoader('//unpkg.com/videojs-contrib-hls.js@3.0.3/dist/videojs-contrib-hlsjs.min.js', {
   //   success: function () {
   //     console.log('load hls done');
