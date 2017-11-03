a136e37e129eed170872103fef1b23bd98803791
anderlu <anderlublaze@gmail.com>
2017-11-03 20:18:55 +0800

    update

---
+++ src/js/tcplayer/plugins/hls-level-switch.js
@@ -0,0 +1,15 @@
+/**
+ * Hls清晰度平滑切换
+ * 依赖hls.js
+ *
+ */
+
+import videojs from '../../video.js';
+
+const Plugin = videojs.getPlugin('plugin');
+
+class HlsLevelSwitch extends Plugin{
+  /**
+   * 切换清晰度
+   */
+}
