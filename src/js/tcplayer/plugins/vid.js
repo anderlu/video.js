ff986e6a8609a08d4599f3f1317fb8124c8531f4
anderlu <anderlublaze@gmail.com>
2017-11-15 11:19:29 +0800

    add async loader

--- src/js/tcplayer/plugins/vid.js
+++ src/js/tcplayer/plugins/vid.js
@@ -4,7 +4,7 @@ const Plugin = videojs.getPlugin('plugin');
 class VID extends Plugin{
   /**
    *
-   * 用于支持传入fileid appid的sources，使用 async-loader 替换原有的 medialoader，异步获取视频地址等信息。
+   * 用于支持传入fileid appid的sources，使用 async-loader 替换原有的 medialoader，在async-loader中异步获取视频地址等信息。
    * {@link async-loader.js}
    *　
    *
@@ -12,8 +12,15 @@ class VID extends Plugin{
   constructor(player, options) {
     super(player);
     console.log('new VID', this, options);
-    player.options_.children[0] = 'MediaAsyncLoader';
-    let playerOptions = player.options_;
+    let sources = player.options_.sources;
+    if(sources.length > 0){
+      for(let i = 0; i < sources.length; i++){
+        if(sources[i].appID && sources[i].fileID){
+          player.options_.children[0] = 'MediaAsyncLoader';
+        }
+      }
+
+    }
   }
   init() {
 
